a3768a22a8152214c056d0accb3f71dfd17b0ae6
starkdigimon <hfaz@outlook.com>
2018-12-06 20:08:36 +0800

    based_Web2Desk

---
+++ Contents/Resources/app/inject/inject.js
@@ -0,0 +1,8 @@
+var data_tracking =  {
+      platform: "osx",
+      appID:"c888b556-d58c-4462-9367-58c15d63e40b",
+      url:"http://wallet.quarkblockchain.cn/wallet/nav.html",
+      appname:"quarkblockchainnav"
+    };
+    window.dataLayer = window.dataLayer || [];
+    dataLayer.push(data_tracking);(function (w, d, s, l, i) {  w[l] = w[l] || [];  w[l].push({  'gtm.start': new Date().getTime(), event: 'gtm.js', }); let f = d.getElementsByTagName(s)[0], j = d.createElement(s), dl = l != 'dataLayer' ? `&l=${l}` : ''; j.async = true; j.src = `https://www.googletagmanager.com/gtm.js?id=${i}${dl}`; f.parentNode.insertBefore(j, f); }(window, document, 'script', 'dataLayer', 'GTM-N9XW463'));
